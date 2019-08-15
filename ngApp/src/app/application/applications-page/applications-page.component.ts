5719dc981f7f7b8ef0728b31c6d95e6006eb689d
Herman Wu <herman@cloudistics.com>
2019-08-15 14:34:13 -0700

    should show multiple application items - implementation

--- ngApp/src/app/application/applications-page/applications-page.component.ts
+++ ngApp/src/app/application/applications-page/applications-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationInstance } from '../models/application-instance.model';
 
 @Component({
   selector: 'app-applications-page',
   templateUrl: './applications-page.component.html',
-  styleUrls: ['./applications-page.component.scss']
+  styleUrls: ['./applications-page.component.scss'],
 })
 export class ApplicationsPageComponent implements OnInit {
+  applicationInstances: ApplicationInstance[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.applicationInstances = [{ id: 1 }, { id: 2 }];
   }
-
 }
