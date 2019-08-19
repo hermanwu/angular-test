ee1d4035723c719452104bc8074ee19b660330fd
Herman Wu <herman@cloudistics.com>
2019-08-18 17:34:19 -0700

    re-org authentication folder

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EventsComponent } from './events/events.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
